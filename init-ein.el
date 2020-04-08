b55ccbdd06b6190fef9caaedda069ae861bf21c1
lishi22692237 <lishi22692237@gmail.com>
2020-04-08 18:02:44 +0800

    add hydra-ein & better keymap

--- init-ein.el
+++ init-ein.el
@@ -5,15 +5,95 @@
 (require 'ein-notebook)
 ;; (require 'ein-subpackages)
 ;; (setq ein:worksheet-warn-obsolesced-keybinding nil)
+;; ==========hydra================
+(defun ein:worksheet-merge-cell-down-km()
+  (interactive)
+  (ein:worksheet-goto-next-input-km)
+  (ein:worksheet-merge-cell-km)
+  )
 
 
 
-(defun myeinkey()
+
+(defhydra hydra-ein()
+ "
+
+
+hydra-ein
+
+_j_: next    _S_: Save   _t_: show      _u_: toggle
+_k_: pre     _R_: Rename _E_: ExAll     _U_: Toggle
+_J_: move    _m_: merge  _l_: Clean     _C_: Exe 
+_K_: Move    _M_: Merge  _L_: CleanAll  _D_: Del
+_o_: InsBl   _c_: Copy   _Q_: Quit      _O_: InsAbo
+_O_: InsAbo  _p_: Paste  _/_: Split
+
+"
+
+;; _j_: next    _S_: Save   _t_: show      _u_: toggle
+;; _k_: pre     _R_: Rename _E_: ExAll     _U_: Toggle
+;; _J_: move    _m_: merge  _l_: Clean     _C_: Exe 
+;; _K_: Move    _M_: Merge  _L_: CleanAll  _D_: Del
+;; _o_: InsBl   _c_: Copy   _Q_: Quit      _O_: InsAbo
+;; _O_: InsAbo  _p_: Paste  _/_: Split
+
+    ("j" ein:worksheet-goto-next-input-km )
+    ("k" ein:worksheet-goto-prev-input-km)
+    ("J" ein:worksheet-move-cell-down-km)
+    ("K" ein:worksheet-move-cell-up-km)
+    ("S" ein:notebook-save-notebook-command-km)
+    ("R" ein:notebook-rename-command-km)
+    ("O" ein:worksheet-insert-cell-above-km )
+    ("o" ein:worksheet-insert-cell-below-km )
+    ("C" ein:worksheet-execute-cell-km)
+    ("m" ein:worksheet-merge-cell-down-km)
+    ("M" ein:worksheet-merge-cell-km)
+    ("c" ein:worksheet-copy-cell-km )
+    ("E" ein:worksheet-execute-all-cells )
+    ("p" ein:worksheet-yank-cell-km )
+    ("t" ein:worksheet-toggle-output-km )
+    ("D" ein:worksheet-kill-cell-km  )
+    ("l" ein:worksheet-clear-output-km )
+    ("L" ein:worksheet-clear-all-output-km )
+    ("Q" ein:notebook-kill-kernel-then-close-command-km)
+    ("/" ein:worksheet-split-cell-at-point-km )
+    ("u" ein:worksheet-toggle-cell-type-km )
+    ("U" ein:worksheet-change-cell-type-km ))
+
+;; (global-set-key (kbd "<f5>") 'hydra-ein/body)
+
+;; ==========hydra================
+
+(defun my-ein-set()
     (interactive)
+    (evil-normal-state)
+
+
+    
 
     (evil-leader/set-key
 	"m." 'hydra-ein/body
 	"mo" 'ein:notebook-open-km
+	"mj" 'ein:worksheet-goto-next-input-km 
+	"mk" 'ein:worksheet-goto-prev-input-km 
+	"mJ" 'ein:worksheet-move-cell-down-km 
+	"mK" 'ein:worksheet-move-cell-up-km 
+	"mS" 'ein:notebook-save-notebook-command-km 
+	"mR" 'ein:notebook-rename-command-km 
+	"mO" 'ein:worksheet-insert-cell-above-km 
+	"mo" 'ein:worksheet-insert-cell-below-km 
+	"mm" 'ein:worksheet-merge-cell-km 
+	"mc" 'ein:worksheet-copy-cell-km 
+	"mC" 'ein:worksheet-execute-all-cells 
+	"mp" 'ein:worksheet-yank-cell-km 
+	"mt" 'ein:worksheet-toggle-output-km
+	"mD" 'ein:worksheet-kill-cell-km  
+	"ml" 'ein:worksheet-clear-output-km 
+	"mL" 'ein:worksheet-clear-all-output-km 
+	"mQ" 'ein:notebook-kill-kernel-then-close-command-km
+	"m/" 'ein:worksheet-split-cell-at-point-km
+	"mu" 'ein:worksheet-toggle-cell-type-km 
+	"mU" 'ein:worksheet-change-cell-type-km
     )
 
 	(which-key-declare-prefixes-for-mode 'python-mode "SPC m " "major-mode")
@@ -25,7 +105,8 @@
 )
 
 
-(add-hook 'ein:notebook-mode-hook 'myeinkey)
+(add-hook 'ein:notebook-mode-hook 'my-ein-set)
+(add-hook 'ein:notebook-mode-hook 'hydra-ein/body)
 
 
 (evil-leader/set-key
@@ -34,32 +115,6 @@
 )
 
 ;; -------------test---------------
-(defhydra hydra-ein() "ein-hydra"
-
-    ("j" ein:worksheet-goto-next-input-km "next")
-    ("k" ein:worksheet-goto-prev-input-km "pre")
-    ("J" ein:worksheet-move-cell-down-km "MoveDown")
-    ("K" ein:worksheet-move-cell-up-km "MoveUp")
-    ("C-s" ein:notebook-save-notebook-command-km "Save")
-    ("R" ein:notebook-rename-command-km "Rename")
-    ("O" ein:worksheet-insert-cell-above-km "InsAbo")
-    ("o" ein:worksheet-insert-cell-below-km "InsBel")
-    ("<return>" ein:worksheet-execute-cell-km "Execut")
-    ("m" ein:worksheet-merge-cell-km "Merge")
-    ("c" ein:worksheet-copy-cell-km "Copy")
-    ("C" ein:worksheet-execute-all-cells "ExAll")
-    ("p" ein:worksheet-yank-cell-km "Paste")
-    ("t" ein:worksheet-toggle-output-km "TogOutput")
-    ("dd" ein:worksheet-kill-cell-km "DelCell" )
-    ("l" ein:worksheet-clear-output-km "clean" )
-    ("L" ein:worksheet-clear-all-output-km "cleanAll" )
-    ("qq" ein:notebook-kill-kernel-then-close-command-km "Quit")
-    ("/" ein:worksheet-split-cell-at-point-km "Split")
-    ("u" ein:worksheet-toggle-cell-type-km "Type")
-    ("U" ein:worksheet-change-cell-type-km "Type")
-)
-
-
 
 
 (setq ein:use-auto-complete t)
