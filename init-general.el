ed4a93ed98123aff180e3a21a691e5b70b57c896
LiShi2269 <lishi22692237@gmail.com>
2020-07-28 15:13:25 +0800

    general changes window

--- init-general.el
+++ init-general.el
@@ -35,6 +35,16 @@
  )
 
  
+(general-define-key
+ :states  '(normal motion )
+ :keymaps 'override 
+ ;; :prefix  "m"
+ (kbd "M-l") 'evil-window-right
+ (kbd "M-h") 'evil-window-left
+ (kbd "M-j") 'evil-window-down
+ (kbd "M-k") 'evil-window-up
+ ;; "l" 'list-bookmarks
+ )
 
 
 (general-define-key
