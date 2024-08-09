2a409bfb631e490b26d5f300cf95f29a3abfd88c
LiShi2269 <lishi22692237@gmail.com>
2024-08-09 10:24:41 +0800

    纠正了一些错误

--- init-org.el
+++ init-org.el
@@ -595,7 +595,7 @@
 (add-hook 'after-init-hook 'org-roam-mode)
 
 
-(setq org-roam-directory "f:/org-roam/")
+(setq org-roam-directory "f:/roam/")
 (org-roam-db-autosync-mode)
 (setq org-roam-v2-ack t)
 
