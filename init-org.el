0e2ea21950512fc8561428100e2ec134ab683dcf
LiShi2269 <lishi22692237@gmail.com>
2025-01-05 22:21:08 +0800

    一些快捷ysnippets

--- init-org.el
+++ init-org.el
@@ -528,14 +528,15 @@
 ;; -------jupyter--------
 (setq org-confirm-babel-evaluate nil)
 
+(add-to-list 'company-backends 'company-ob-ipython) ;补全模式启动
 (require 'ob-ipython)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
  '(
    (emacs-lisp . t)
-   (python . t)
-   ;; (ipython . t)
+   ;; (python . t)
+   (ipython . t)
    ;;(jupyter . t)
    ))
 
@@ -545,6 +546,57 @@
                                                     ;; (:kernel . "julia-1.0")
 						    ))
     
+
+
+
+
+
+
+
+(defvar org-babel-eval-verbose t
+  "A non-nil value makes `org-babel-eval' display")
+
+(defun org-babel-eval (command query)
+  "Run COMMAND on QUERY.
+Writes QUERY into a temp-buffer that is processed with
+`org-babel--shell-command-on-region'.  If COMMAND succeeds then return
+its results, otherwise display STDERR with
+`org-babel-eval-error-notify'."
+  (let ((error-buffer (get-buffer-create " *Org-Babel Error*")) exit-code)
+    (with-current-buffer error-buffer (erase-buffer))
+    (with-temp-buffer
+      (insert query)
+      (setq exit-code
+        (org-babel--shell-command-on-region
+         command error-buffer))
+
+      (if (or (not (numberp exit-code)) (> exit-code 0)
+              (and org-babel-eval-verbose (> (buffer-size error-buffer) 0)))
+      (progn
+        (with-current-buffer error-buffer
+          (org-babel-eval-error-notify exit-code (buffer-string)))
+        (save-excursion
+          (when (get-buffer org-babel-error-buffer-name)
+        (with-current-buffer org-babel-error-buffer-name
+          (unless (derived-mode-p 'compilation-mode)
+            (compilation-mode))
+          ;; Compilation-mode enforces read-only, but Babel expects the buffer modifiable.
+          (setq buffer-read-only nil))))
+        nil)
+    (buffer-string)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ;;========== 出现 org-babel-execute-src-block: No org-babel-execute function 问题==========================================
 ;; jupyter-run-repl
