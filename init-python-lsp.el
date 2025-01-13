b1c9264ac70d9b4ab00a3529113f1f06c6127f6e
LiShi2269 <lishi22692237@gmail.com>
2025-01-13 22:04:05 +0800

    目前来看python的lsp用不了

--- init-python-lsp.el
+++ init-python-lsp.el
@@ -194,12 +194,10 @@
 
 ;; lsp-completion-mode会导致不能在lspmode中补全路径
 (add-hook 'lsp-mode-hook (lambda () (flycheck-mode 1)(hs-minor-mode 1)(lsp-completion-mode 0)))
-(setq lsp-python-ms-python-executable "c:/Program Files/Python312/python.exe")
-(setq lsp-python-ms-extra-paths  '("c:/Program Files/Python312/python.exe"))
 ;; 使用pyenv的时候
-;; (setq lsp-python-ms-python-executable  "C:/HOME/.pyenv/pyenv-win/versions/3.9.2/python.exe")
-;; (setq lsp-python-ms-extra-paths  '("C:/HOME/.pyenv/pyenv-win/versions/3.9.2/python.exe" ))
-
+(setq lsp-python-ms-python-executable  "c:/Users/lishi/.pyenv/pyenv-win/versions/3.13.1/python.exe")
+(setq lsp-python-ms-extra-paths  '("c:/Users/lishi/.pyenv/pyenv-win/versions/3.13.1/python.exe" ))
+(setq elpy-rpc-python-command "python")
 
 (add-hook 'hack-local-variables-hook
       (lambda ()
